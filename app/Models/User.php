dddf25badd54ba403ac1227f92e586b61f01da3c
GFONOLLOSA <98556518+Flogaberic@users.noreply.github.com>
2024-09-04 23:33:54 +0200

    tuto premieres tables

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,8 +17,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'github_id',
         'name',
         'email',
+        'profile_picture_url',
+        'locale',
+        'theme',
         'password',
     ];
 
@@ -31,6 +35,12 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function pokemons()
+    {
+    return $this->belongsToMany(Pokemon::class)
+                ->withPivot('created_at');
+    }
 
     /**
      * Get the attributes that should be cast.
